8ec63bfe8d9c317bdb7549f031ee6255d9a41293
Ghassan Alaydi <ghassan_aidi@hotmail.com>
2020-09-21 17:07:15 +0200

    animated header

---
+++ MyPortfolio/wwwroot/js/defaultPage.js
@@ -0,0 +1,20 @@
+﻿(function defaultHeaderTyping() {
+    const text = 'Welcome To My Humble Portfolio';
+    defaultHeader = document.querySelector("header h1");
+    defaultHeaderAfter = document.queryCommandEnabled("header h1::after");
+    let index = 0;
+    let currentText = '';
+    if (screen.width < 760) {
+        defaultHeader.textContent = text;
+        defaultHeaderAfter.style.border = "solid 5px red";
+    }
+    else {
+        (function type() {
+            currentText = text.slice(0, ++index);
+            defaultHeader.textContent = currentText;
+            if (currentText !== text) {
+                setTimeout(type, 100);
+            }
+        }());
+    }
+}());
