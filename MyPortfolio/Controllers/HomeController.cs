ad3f72782c51d7fcb22c39268f4ee2a54f8af9a2
Ghassan Alaydi <ghassan_aidi@hotmail.com>
2020-10-07 14:57:15 +0200

    Work in progress

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -23,11 +23,11 @@ namespace MyPortfolio.Controllers
             return View(model);
         }
 
-        public ViewResult Details()
+        public ViewResult Details(int id)
         {
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(1),
+                Employee = _employeeRepository.GetEmployee(id),
                 PageTitle = "Employee Details"
             };
             return View(homeDetailsViewModel);
