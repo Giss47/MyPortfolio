b78a8cb1fbb5e92eb389d984c71da2d2777be69c
Ghassan Alaydi <ghassan_aidi@hotmail.com>
2020-10-08 12:26:05 +0200

    Added validation for create form

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -40,10 +40,15 @@ namespace MyPortfolio.Controllers
         }
 
         [HttpPost]
-        public RedirectToActionResult Create(Employee employee)
+        public IActionResult Create(Employee employee)
         {
-            Employee newEmployee = _employeeRepository.Add(employee);
-            return RedirectToAction("details", new { id = employee.Id });
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
         }
     }
 }
