ea5d17264ec7ac0852f383b6b1309f7d8f23b46e
Ghassan Alaydi <ghassan_aidi@hotmail.com>
2020-10-14 19:14:03 +0200

    Udo upload photso to Azure

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -27,8 +27,6 @@ namespace MyPortfolio.Controllers
 
         public ViewResult List()
         {
-            ViewBag.WebRootpath = _webHostEnvironment.WebRootPath;
-            ViewBag.ContentRootPath = _webHostEnvironment.ContentRootPath;
             var model = _employeeRepository.GetAllEmployees();
             return View(model);
         }
@@ -57,9 +55,9 @@ namespace MyPortfolio.Controllers
                 string uniqueFileName = null;
                 if (model.Photo != null)
                 {
-                    var uploadsFodler = System.Environment.SpecialFolder.CommonApplicationData;
+                    string uploadsFodler = Path.Combine(_webHostEnvironment.WebRootPath, "images\\employees");
                     uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                    string filePath = Path.Combine(uploadsFodler.ToString(), uniqueFileName);
+                    string filePath = Path.Combine(uploadsFodler, uniqueFileName);
                     FileStream fileStream = new FileStream(filePath, FileMode.CreateNew);
                     model.Photo.CopyTo(fileStream);
                     fileStream.Dispose();
@@ -74,7 +72,7 @@ namespace MyPortfolio.Controllers
                 };
 
                 _employeeRepository.Add(newEmployee);
-                return RedirectToAction("Details", newEmployee.Id);
+                return RedirectToAction("Details", new { id = newEmployee.Id });
             }
 
             return View();
